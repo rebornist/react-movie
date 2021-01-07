21a6f4c3c44dbe8646fa93c25e01bb0e4146de85
sshs0702 <rebornist@naver.com>
2021-01-07 10:29:03 +0900

    jsx section complate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import Potato from './Potato';
+// import Potato from './Potato';
+
+
+function Food({ fav }) {
+  return <h1>I like { fav }</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!!</h1>
       world
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="chukumi" />
     </div>
   );
 }
