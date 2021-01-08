19e8e9dba4335179fe0e25ee3df9c3de2e6a936c
sshs0702 <rebornist@naver.com>
2021-01-08 09:01:54 +0900

    planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
 
   state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState(current => ({count: current.count+1}));
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count-1}));
+    isLoading: true
   };
 
   componentDidMount(){
-    console.log("component rendered");
-  }
-
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-
-  componentWillUnmount(){
-    console.log("good bye");
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000
+    )
   }
 
   render() {
-    console.log("render");
+    const {isLoading} = this.state;
     return (
     <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
+      {isLoading ? "Loading...": "We are ready"}
     </div>
     );
   }
---
+++ src/state.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
+
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({count: current.count+1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count-1}));
+  };
+
+  componentDidMount(){
+    console.log("component rendered");
+  }
+
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+
+  componentWillUnmount(){
+    console.log("good bye");
+  }
+
+  render() {
+    console.log("render");
+    return (
+    <div>
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
+    );
+  }
+}
+
+export default App;
